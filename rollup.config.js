7fdef492860c8cd59c14d2ed0c7c255d25745afb
Sir Code <44554976+MisterSirCode@users.noreply.github.com>
2021-01-15 10:53:21 -0500

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,3 @@
-/**
- * dat-gui JavaScript Controller Library
- * https://github.com/dataarts/dat.gui
- *
- * Copyright 2011 Data Arts Team, Google Creative Lab
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- */
-
 import fs from 'fs';
 import path from 'path';
 import resolve from 'rollup-plugin-node-resolve';
@@ -21,17 +8,17 @@ import sass from 'rollup-plugin-sass';
 const banner = fs.readFileSync(path.join(__dirname, 'licenseBanner.txt'));
 
 export default {
-  input: 'src/dat/index.js',
+  input: 'src/j/index.js',
   output: [{
     // TODO: Remove default exports, and this line, in v0.8.0.
     exports: 'named',
-    file: './build/dat.gui.js',
+    file: './build/j.gui.js',
     format: 'umd',
-    name: 'dat',
+    name: 'j',
     sourcemap: true,
     banner: banner
   }, {
-    file: './build/dat.gui.module.js',
+    file: './build/j.gui.module.js',
     format: 'es',
     sourcemap: true,
     banner: banner
@@ -43,7 +30,7 @@ export default {
     resolve(),
     sass({
       insert: true,
-      output: 'build/dat.gui.css',
+      output: 'build/j.gui.css',
       options: {outputStyle: 'compressed'}
     }),
     babel({
